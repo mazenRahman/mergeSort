9d84b07a68cd8329987166f2ad91fefc9c94ca7e
mazenRahman <mazenarshad02@gmail.com>
2024-11-02 14:07:11 -0500

    Initial commit

---
+++ mergeSort.py
@@ -0,0 +1,40 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Find the middle point to divide the array into two halves
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursive call on each half
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Merging the two halves
+        i = j = k = 0
+
+        # Copy data to temporary arrays left_half[] and right_half[]
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left in the left_half
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        # Checking if any element was left in the right_half
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Sample usage
+arr = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(arr)
+print("Sorted array:", arr)
